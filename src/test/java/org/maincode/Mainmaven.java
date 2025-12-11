4ce80d3897fdcd3556f671df355299548e8376e7
lakshmikcm <lakshmi.kcm@gmail.com>
2025-12-11 20:38:25 +0530

    Added my mini project

---
+++ src/test/java/org/maincode/Mainmaven.java
@@ -0,0 +1,38 @@
+package org.maincode;
+
+import org.login.Registration;
+import org.login.login;
+
+public class Mainmaven extends Registration{
+	
+	public static void main(String[] args) {
+		
+		browserLaunch("chrome");
+		implicitWait(20);
+		urlLaunch("https://adactinhotelapp.com/");
+		
+		//create object for login page
+		login l=new login();
+		sendKeys(l.getUser(),"Kalai");
+		sendKeys(l.getPass(),"123456");
+		click(l.getLoginbtn());
+		
+		//create object for registration page
+		Registration r=new Registration();
+		
+		click(r.getCreateAct());
+		sendKeys(r.getUsername(),"Kalaiarasi");
+		sendKeys(r.getPassword(),"123456");
+		sendKeys(r.getRepass(),"123456");
+		sendKeys(r.getFullname(),"Kalaivani");
+		sendKeys(r.getEmail(),"kalai@gmail.com");
+		click(r.getCheck());
+		
+		click(r.getSubmit());
+		
+	
+		
+		
+	}
+
+}
